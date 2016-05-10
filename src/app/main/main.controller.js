e320c1dc9261bd09ef9a23f6fc1a28482ffda1ef
alexlashford <alexl@cakesolutions.net>
2016-05-10 23:57:43 +0100

    added carosel, and first cut of backing data model

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,8 +9,29 @@
   function MainController() {
     var vm = this;
 
-    vm.creationDate = 1462648970556;
+    var genre = {
+      'pop'  : [{id:1, image:'http://lorempixel.com/' + 601 + '/300', artist:'Michal Jackson', title:'Bad'},
+                {id:2, image:'http://lorempixel.com/' + 602 + '/300', artist:'Sting', title:'Fields of Barley'}],
+      'rock' : [{id:3, image:'http://lorempixel.com/' + 601 + '/300', artist:'Nickleback', title:'Rockstar'},
+                {id:4, image:'http://lorempixel.com/' + 602 + '/300', artist:'Foo Fighters', title:'Breakout'}]
+    };
 
+    vm.genreKeys = Object.keys(genre);
+    vm.creationDate = 1462648970556;
+    vm.selectedGenre = genre['pop'];
+
+    vm.setGenre = function(key) {
+      vm.selectedGenre = genre[key];
+    };
+
+    // function addSlide() {
+    //   var newWidth = 600 + slides.length + 1;
+    //   slides.push({
+    //     image: 'http://lorempixel.com/' + newWidth + '/300',
+    //     text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
+    //     id: currIndex++
+    //   });
+    // }
 
   }
 })();
